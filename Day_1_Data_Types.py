ac83d04373579f80c4f74d9fa780a9fff9aba95b
Flesier Akinyi <flesieronyango18@gmail.com>
2023-02-15 21:14:00 +0300

    Update Day_1_Data_Types.py

--- Day_1_Data_Types.py
+++ Day_1_Data_Types.py
@@ -7,6 +7,9 @@ b = 4.0
 c = 'is the best place to learn and practice coding!'
 
 # Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = input()
 
 # Print the sum of both integer variables on a new line.
 print(i + a)
@@ -15,3 +18,4 @@ print(d + b)
 # Concatenate and print the String variables on a new line
 print(s + c)
 # The 's' variable above should be printed first.
+
